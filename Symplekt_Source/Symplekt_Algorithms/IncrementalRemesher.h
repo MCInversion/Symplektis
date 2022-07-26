467ca9406054856c94ec340acb3c79ccddd8cea9
Martin Cavarga <martin.cavarga@hyperganic.com>
2022-07-26 18:36:26 +0200

    Preparing files for remeshing functionality for referenced meshes

---
+++ Symplekt_Source/Symplekt_Algorithms/IncrementalRemesher.h
@@ -0,0 +1,17 @@
+/*!  \file IncrementalRemesher.h
+ *   \brief An object for incremental remeshing of a ReferencedMeshGeometry instance.
+ *
+ *   \author M. Cavarga (MCInversion)
+ *   \date   26.7.2022
+*/
+
+#pragma once
+
+namespace Symplektis::Algorithms
+{
+	class IncrementalRemesher
+	{
+	public:
+
+	};
+} // namespace Symplektis::Algorithms
